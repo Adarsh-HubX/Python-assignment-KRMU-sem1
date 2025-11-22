2561f10d40a7b4355e79fc64ef3d4a7b36655c8f
Adarsh Rathore <adarshrathore.sec@gmail.com>
2025-11-22 14:18:53 +0530

    Update book.py

--- lab3/library-inventory-manager/library_manager/book.py
+++ lab3/library-inventory-manager/library_manager/book.py
@@ -1,3 +1,14 @@
+# --------------------------------------------------------------------
+# Name        : Adarsh Rathore
+# Roll No     : <2501410014>
+# Course      : B.Tech CSE (Cyber Security)
+# Semester    : 1st Semester
+# Subject     : Programming for Problem Solving Using Python
+# File        : main.py
+# Project     : Library Inventory Manager
+# --------------------------------------------------------------------
+
+
 class Book:
     def __init__(self, title, author, isbn, status="available"):
         self.title = title
@@ -29,4 +40,5 @@ class Book:
     def return_book(self):
         if self.is_available():
             raise ValueError("This book is not issued.")
+
         self.status = "available"
