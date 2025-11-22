1b2e1d42c7dc6e33d85b30ea54787be325014f18
Adarsh Rathore <adarshrathore.sec@gmail.com>
2025-11-22 13:27:33 +0530

    Add files via upload

---
+++ lab3/library-inventory-manager/book.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self, title, author, isbn, status="available"):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.status = status
+
+    def __str__(self):
+        return f"Title: {self.title} | Author: {self.author} | ISBN: {self.isbn} | Status: {self.status}\n"
+
+
+    
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn,
+            "status": self.status
+        }
+    
+    def is_available(self):
+        return self.status == "available"
+    
+    def issue(self):
+        if not self.is_available():
+            raise ValueError("This book is already issued.")
+        self.status = "issued"
+    
+    def return_book(self):
+        if self.is_available():
+            raise ValueError("This book is not issued.")
+        self.status = "available"
